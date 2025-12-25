756a02df0807908d94fbefceceb0168c26044934
Micro-Prog <sajjadsttr01@gmail.com>
2025-12-25 05:46:16 +0330

    v0.0.1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.tsx'
+import './App.css'
+
+import './index.css'
+import 'antd/dist/reset.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
