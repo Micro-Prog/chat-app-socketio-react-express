2e9e092215a64782c4683cf73c94fc2de7bd0997
Micro-Prog <sajjadsttr01@gmail.com>
2025-12-30 21:05:08 +0330

    v0.0.4

--- frontend/src/lib/types/user/user.ts
+++ frontend/src/lib/types/user/user.ts
@@ -1,4 +1,5 @@
 export interface User {
+  bio: ReactNode;
   _id: string;
   fullName: string;
   profilePic?: string;
--- frontend/src/pages/hompage/components/rightsidebar/RightSideBar.tsx
+++ frontend/src/pages/hompage/components/rightsidebar/RightSideBar.tsx
@@ -1,14 +1,42 @@
+import assets, { imagesDummyData } from '@/lib/assets/assets';
 import type { SideBarProps } from '@/lib/types/sidebar/sidebar';
 import React from 'react'
 
 
 
 const RightSideBar:React.FC<SideBarProps> = ({ 
-  // selectedUser, setSelectedUser 
+  selectedUser, setSelectedUser 
 } ) => {
-  return (
-    <div>
-      right Side Bar
+  return selectedUser && (
+    <div className={`bg-bgColor-dark text-white w-full relative overflow-y-scroll
+    ${selectedUser ? "max-md:hidden" : ""}`}>
+      <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt=''
+        className='w-20 aspect-[1/1] rounded-full' />
+        <h1 className='px-10 text-xl gap-2 font-medium mx-auto flex items-center gap-0'>
+          {selectedUser.fullName}
+          <span className='w-2 h-2 rounded-full bg-textColor-online'></span>
+        </h1>
+        <p className='px-10 mx-auto'>{selectedUser.bio}</p>
+      </div>
+
+      <hr className='border-borderColor-tertiary my-4' />
+
+      <div className='px-5 text-xs'>
+        <p>Media</p>
+        <div className='mt-2 mb-10 max-h-[200px] overflow-y-scroll grid grid-cols-2
+        gap-4 opacity-80'>
+          {imagesDummyData.map((url, index) => (
+            <div key={index} onClick={() => window.open(url)} 
+            className='cursor-pointer rounded'
+            >
+              <img src={url} alt='' className='h-full rounded-md' />
+            </div>
+          ))}
+        </div>
+      </div>
+
+
     </div>
   )
 }
